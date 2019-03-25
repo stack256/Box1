b49224b87dd8dad19ccd6541b225d2d6b62a25a0
syakubov <syakubov@blogic.ru>
2019-03-25 15:31:15 +0500

    Обновить 'src/test/java/Box/About.java'

--- src/test/java/Box/About.java
+++ src/test/java/Box/About.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.logging.LogEntries;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
